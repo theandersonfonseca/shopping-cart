ee5789f1897b9941d95d7ce3a1dd361d9c98c539
Anderson Fonseca <andersonfonseca1999@hotmail.com>
2021-05-13 18:06:13 -0300

    add project initial setup

---
+++ src/styles/global.ts
@@ -0,0 +1,57 @@
+import { createGlobalStyle } from 'styled-components';
+import { css } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    box-sizing: border-box;
+    vertical-align: baseline;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+
+    &::before,
+    &::after {
+      box-sizing: inherit;
+    }
+  }
+
+  html {
+    font-size: 62.5%; 
+  }
+
+  body {
+    ${({ theme }) => css` 
+      font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+      sans-serif;
+      min-height: 100vh;
+      text-rendering: optimizeSpeed;
+      line-height: 1.5;
+      background: ${theme.colors.primary};
+      display: flex;
+      justify-content: center;
+      align-items: center;
+  `}
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  img,
+  picture {
+    max-width: 100%;
+    display: block;
+  }
+
+  input,
+  button,
+  textarea,
+  select {
+    font: inherit;
+  }
+`;
+
+export default GlobalStyle;
---
+++ src/styles/theme.ts
@@ -0,0 +1,55 @@
+export default {
+  colors: {    
+    primary: '#5a9b67',
+    secondary: ' #cf5856',
+    white: '#FAFAFA',
+    dark: '#333',
+    gray: '#928d8d',
+    grayLight: '#eee',
+  },
+
+  font: {
+    light: 300,
+    normal: 400,
+    bold: 600,
+  },
+
+  sizes: {
+    xsmall: '1.2rem',
+    small: '1.4rem',
+    medium: '1.6rem',
+    large: '1.8rem',
+    xlarge: '2.0rem',
+    xxlarge: '2.8rem',
+    huge: '4rem'
+  },
+
+  spacings: {
+    xxsmall: '0.8rem',
+    xsmall: '1.6rem',
+    small: '2.4rem',
+    medium: '3.2rem',
+    large: '4.0rem',
+    xlarge: '4.8rem',
+    xxlarge: '5.6rem'
+  },
+
+  layers: {
+    base: 10,
+    menu: 20,
+    overlay: 30,
+    modal: 40,
+    alwaysOnTop: 50
+  },
+
+  transition: {
+    default: '0.3s ease-in-out',
+    fast: '0.1s ease-in-out'
+  },
+
+  border: {
+    radius: '0.4rem'
+  },
+} as const
+
+
