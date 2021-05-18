3bbdc2807e3583781d28ebb1690a31c369269812
Anderson Fonseca <andersonfonseca1999@hotmail.com>
2021-05-17 22:09:41 -0300

    add App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from './styles/global';
+import theme from './styles/theme';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Header from './components/Header';
+import Products from './components/Products';
+
+import { ContextProvider } from './state/context';
+
+const App = () => (
+  <ThemeProvider theme={theme}>
+    <ContextProvider>
+      <Header/>
+      <Products/>
+    </ContextProvider>
+    <GlobalStyle/>
+  </ThemeProvider>
+);
 
 export default App;
